8a6042f5a81b9ca18979545251316d4fee63a0e7
rowida22 <rowida91200@gmail.com>
2023-02-04 00:06:41 +0200

    Dmn linktext

--- src/test/java/TestDemo/test.java
+++ src/test/java/TestDemo/test.java
@@ -1,7 +1,6 @@
 package TestDemo;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
@@ -24,38 +23,45 @@ public class test {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 
         driver.get("https://google.com/");
-        //String firstTab = driver.getWindowHandle(); // Switching
-        driver.navigate().to("https://facebook.com/");
+        // String firstTab = driver.getWindowHandle(); // Switching
+        // driver.navigate().to("https://facebook.com/");
 
         /*
          * Add Some Action and print it in terminal
          * getURL , print title and other navigation
          */
 
-        System.out.println(driver.getCurrentUrl());
-        System.out.println(driver.getTitle());
+        // System.out.println(driver.getCurrentUrl());
+        // System.out.println(driver.getTitle());
 
-        
-        //  driver.navigate().back();
-        //  Thread.sleep(2000);
+        // driver.navigate().back();
+        // Thread.sleep(2000);
 
-        //  driver.navigate().forward();
-        //  Thread.sleep(2000);
-        //  driver.navigate().refresh();
+        // driver.navigate().forward();
+        // Thread.sleep(2000);
+        // driver.navigate().refresh();
 
-        
-         /* Swithing to the original tabs
-         open it in new tab or window */
-        
+        /*
+         * Swithing to the original tabs
+         * open it in new tab or window
+         */
 
         // driver.switchTo().newWindow(WindowType.WINDOW);
         // driver.switchTo().newWindow(WindowType.TAB);
         // driver.switchTo().window(firstTab);
 
         driver.navigate().to("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html");
+        driver.switchTo().frame("packageFrame");
         driver.findElement(By.linkText("Alert")).click();
-        // driver.close(); //Close only the last openned tab
-        driver.quit(); // Close the entire browser
+
+        // driver.get("https://www.selenium.dev/selenium/docs/api/java/index.html?overview-summary.html");
+        // WebElement frame = driver.findElement(By.name("packageFrame"));
+        // driver.switchTo().frame(frame);
+        // Thread.sleep(1000);
+        // driver.findElement(By.linkText("Action")).click();
+        // Thread.sleep(3000);
+        // driver.close(); // Close only the last openned tab
+        // driver.quit(); // Close the entire browser
 
     }
 
