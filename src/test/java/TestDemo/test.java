24a6983880ae5675c8f39621ec9a52118169d492
rowida22 <rowida91200@gmail.com>
2023-02-03 19:54:18 +0200

    Add depen instead of webdriver

--- src/test/java/TestDemo/test.java
+++ src/test/java/TestDemo/test.java
@@ -1,5 +1,6 @@
 package TestDemo;
 
+import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,11 +8,18 @@ public class test {
 
     public static void main(String[] args) {
 
-        //System.setProperty("webdriver.chrome.driver", "I:\\Test2\\Test2\\.\\driver\\chromedriver.exe");
-        
+        // System.setProperty("webdriver.chrome.driver",
+        // "I:\\Test2\\Test2\\.\\driver\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().scriptTimeout(Duration.ofMinutes(2));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
         driver.get("https://facebook.com/");
+
+        driver.close();
 
     }
 
