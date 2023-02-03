c20cf2056f8bdafcfc2308e0301567c7487c6426
rowida22 <rowida91200@gmail.com>
2023-02-03 21:39:48 +0200

    +Actions

--- src/test/java/TestDemo/test.java
+++ src/test/java/TestDemo/test.java
@@ -7,10 +7,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
          * Simple openning & close a site with implicit time
-         * Add some Actions
+         * 
          */
 
         WebDriverManager.chromedriver().driverVersion("109.0.5414.74").setup();
@@ -22,9 +22,20 @@ public class test {
 
         driver.get("https://google.com/");
         driver.navigate().to("https://facebook.com/");
+        /* Add Some Action and print it in terminal 
+         * getURL , print title and other navigation
+        */
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
 
+        driver.navigate().back();
+        Thread.sleep(2000);
+        
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        
+        Thread.sleep(2000);
         driver.close();
 
     }
